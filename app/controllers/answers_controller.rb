fed48bb3c229af1e60ce5416cd4a817929f91d85
shyamabacousnac <shyam.abacousnac@gmail.com>
2021-09-21 20:45:38 +0400

    coach

---
+++ app/controllers/answers_controller.rb
@@ -0,0 +1,13 @@
+class AnswersController < ApplicationController
+    def answer
+        @question = params[:question]
+
+        if @question == "I am going to work right now!"
+            return @answer = ""
+        elsif @question.include?("?")
+            return @answer = "Silly question, get dressed and go to work!"
+        else
+            return @answer = "I don't care, get dressed and go to work!"
+        end
+    end
+end
